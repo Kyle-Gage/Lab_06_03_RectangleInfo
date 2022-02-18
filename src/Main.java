699022385298d1b7beec3cb43030677656ce3434
Kyle-Gage <gageka@mail.uc.edu>
2022-02-18 12:00:48 -0500

    Final Code

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,22 @@ public class Main
 
     public static void main(String[] args)
     {
-	Scanner in = new Scanner(System.in);
-    int sideLeft = 0;
-    int sideTop = 0;
-    int sideRight = 0;
-    int sideBottom = 0;
-    double recArea;
-    double recPerim;
-    double recDiagonal;
-    double pythTheory;
-    String trash;
+        Scanner in = new Scanner(System.in);
+        double height = 0;
+        double width = 0;
+        double recArea; // height * width
+        double recPerim; // 2 * (height + width)
+        double recDiagonal; // c = square root (height^2 + width^2)
+        double pythTheory;
+        String trash;
 
 
 
-        System.out.print("Enter the length for Left side: ");
+        System.out.print("Enter the length for hieght: ");
 
-        if (in.hasNextInt())
+        if (in.hasNextDouble())
         {
-            sideLeft = in.nextInt();
+            height = in.nextInt();
             in.nextLine(); // Clear the input buffer
         }
         else
@@ -33,11 +31,11 @@ public class Main
             System.exit(0);
         }
 
-        System.out.print("Enter the length for Top side: ");
+        System.out.print("Enter the length for width: ");
 
         if (in.hasNextInt())
         {
-            sideTop = in.nextInt();
+            width = in.nextInt();
             in.nextLine(); // Clear the input buffer
         }
         else
@@ -48,41 +46,12 @@ public class Main
             System.exit(0);
         }
 
-        System.out.print("Enter the length for Right side: ");
 
-        if (in.hasNextInt())
-        {
-            sideRight = in.nextInt();
-            in.nextLine(); // Clear the input buffer
-        }
-        else
-        {
-            trash = in.nextLine();
-            System.out.println("\n You must enter a valid integer and not " + trash);
-            System.out.println("Run the program again!");
-            System.exit(0);
-        }
 
-        System.out.print("Enter the length for Bottom Side: ");
 
-        if (in.hasNextInt())
-        {
-            sideBottom = in.nextInt();
-            in.nextLine(); // Clear the input buffer
-
-        }
-        else
-        {
-            trash = in.nextLine();
-            System.out.println("\n You must enter a valid integer and not " + trash);
-            System.out.println("Run the program again!");
-            System.exit(0);
-        }
-
-        recArea = sideTop * sideLeft;
-        recPerim = sideLeft + sideTop + sideRight + sideBottom;
-        pythTheory = ((sideLeft * sideLeft) + (sideBottom * sideBottom));
-        recDiagonal = Math.sqrt(pythTheory);
+        recArea = width * height;
+        recPerim = (height + width) * 2;
+        recDiagonal = Math.sqrt((height * height) + (width * width));
 
         System.out.println("\n The area of the rectangle is " + recArea + " and the perimeter is " + recPerim);
         System.out.println("\n The legnth of the diagonal is " + recDiagonal);
